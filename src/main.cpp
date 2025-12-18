d6e5ab2ebe06c17049a7d704521da914c88f6210
egogoboy <egor2005sudakov@gmail.com>
2025-12-18 17:04:14 +0300

    feat: add font, change bg color

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML 3 Test");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "typedefence");
 
     while (window.isOpen()) {
         while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) window.close();
         }
 
-        window.clear(sf::Color::Black);
+        window.clear(sf::Color(219, 216, 208));
         window.display();
     }
 }
